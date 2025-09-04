75f047ea604a370569d9e93ffb1d596263a1c9b2
Nick Eubank <nick@nickeubank.com>
2025-09-04 09:53:40 -0400

    upload example

---
+++ ids590_specific/lecture_slides/debugger_example_code.py
@@ -0,0 +1,32 @@
+"""Debugger exercise."""
+
+from statistics import mean
+
+
+def sum_numbers(list_to_sum):
+    """Given a list of numbers, add up the numbers and return total"""
+    total = 0
+    for i in range(1, len(list_to_sum)):
+        total += list_to_sum[i]
+    return total
+
+
+def get_mean_of_list(list_to_sum):
+    """Calculate mean of numbers in a list"""
+    total_of_list = sum_numbers(list_to_sum)
+    number_of_numbers = len(list_to_sum)
+    list_mean = total_of_list / number_of_numbers
+    return list_mean
+
+
+def main():
+    # Test Case
+    my_list = [12, 32, 1, 23, 32]
+    print(f"The mean of {my_list} should be {mean(my_list):.2f}")
+
+    mean_value = get_mean_of_list(my_list)
+
+    print(f"The value from my function is is {mean_value:.2f}")
+
+
+main()
