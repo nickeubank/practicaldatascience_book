dd31d54a3cb1cffb52352c2bce47c46f79285346
Nick Eubank <nick@nickeubank.com>
2025-09-18 13:26:06 -0400

    make 5990 debugger easier

--- Example_Data/debugger_590.py
+++ Example_Data/debugger_590.py
@@ -3,19 +3,31 @@
 
 def split_string(string):
     """Given a string, split it into a list of 'words'."""
-    start_of_word = 0
-    words = []
-    for i in range(len(string)):
-        if string[i] == " ":
-            word = string[start_of_word:i]
+    # Remove last period
+    string = string[:-1]
 
-            # If starts with a space, drop that space.
-            if word[0] == " ":
-                word = word[1:]
-
-            words.append(word)
-            start_of_word = i
+    # Split
+    words = string.split()
     return words
+
+
+def get_repeats(sentence_as_list):
+    unique_words = []
+    repeated_words = []
+    for i in range(1, len(sentence_as_list)):
+        # If we haven't see it before, put in
+        # unique words list.
+        word = sentence_as_list[i]
+        if word not in unique_words:
+            unique_words.append(word)
+
+        # If we have seen it before (it's in
+        # the unique words list),
+        # it is a repeat and we put it in
+        # repeated words if it isn't already there.
+        elif word in unique_words and word not in repeated_words
+            repeated_words.append(word)
+    return repeated_words
 
 
 def get_repeated_words(sentence):
@@ -25,21 +37,7 @@ def get_repeated_words(sentence):
     sentence_as_list = split_string(sentence)
 
     # Figure out what is repeated
-    unique_words = []
-    repeated_words = []
-    for word in sentence_as_list:
-        # If we haven't see it before, put in
-        # unique words list.
-        if word not in unique_words:
-            unique_words.append(word)
-
-        # If we have seen it before (it's in
-        # the unique words list),
-        # it is a repeat and we put it in
-        # repeated words if it isn't already there.
-        elif word in unique_words word not in repeated_words:
-            repeated_words.append(word)
-
+    repeated_words = get_repeats(sentence_as_list)
     return repeated_words
 
 
@@ -49,23 +47,7 @@ def main():
     print("Test Case 0:")
 
     sentence = (
-        "Far out in the uncharted backwaters of the "
-        "unfashionable end of the western spiral arm of "
-        "the Galaxy lies a small unregarded yellow sun."
-    )
-
-    print(f"The sentence is: {sentence}")
-    print("The result should be  ['the', 'of']")
-
-    test_words = get_repeated_words(sentence)
-
-    print(f"Result is: {test_words}")
-
-    # Test Case 1
-    print("Test Case 1:")
-
-    sentence = (
-        "You should always be yourself unless you can "
+        "you should always be yourself unless you can "
         "be Batman then you should always be Batman."
     )
 
@@ -74,6 +56,22 @@ def main():
 
     test_word = get_repeated_words(sentence)
     print(f"Result is: {test_word}")
+
+    # Test Case 1
+    print("Test Case 1:")
+
+    sentence = (
+        "far out in the uncharted backwaters of the "
+        "unfashionable far end of the western spiral arm of "
+        "the Galaxy lies a small unregarded yellow sun."
+    )
+
+    print(f"The sentence is: {sentence}")
+    print("The result should be  ['the', 'far', 'of']")
+
+    test_words = get_repeated_words(sentence)
+
+    print(f"Result is: {test_words}")
 
 
 main()
