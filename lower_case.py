9fc0a7e7bb15d3258d36d224cfe3bbf1302e853e
Nick Eubank <nick@nickeubank.com>
2025-10-08 15:44:59 -0400

    make upper case again

---
+++ lower_case.py
@@ -0,0 +1,26 @@
+import glob
+import os
+
+# Define the directories to search
+directories = ["ids590_specific/exercises_590", "ids720_specific/exercises"]
+
+# Find and rename files in each directory
+for directory in directories:
+    # Find all .ipynb files starting with "Exercise_"
+    pattern = os.path.join(directory, "exercise_*.ipynb")
+    files = glob.glob(pattern)
+
+    for file_path in files:
+        # Get the directory and filename separately
+        dir_name = os.path.dirname(file_path)
+        file_name = os.path.basename(file_path)
+
+        # Create new filename with lowercase 'e'
+        new_file_name = file_name.replace("exercise_", "Exercise_", 1)
+        new_file_path = os.path.join(dir_name, new_file_name)
+
+        # Rename the file
+        print(f"Renaming: {file_path} -> {new_file_path}")
+        os.rename(file_path, new_file_path)
+
+print("Done!")
